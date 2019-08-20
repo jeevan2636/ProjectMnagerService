49054ad035494297757d08575edabcf6c3d12c4a
jeevan2636 <jeevan.b2@cognizant.com>
2019-08-20 03:28:55 -0700

    Commit

---
+++ ProjectManager.PerformanceTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectManager.PerformanceTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
